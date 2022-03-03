63dc3d20f764f10ba33c61d0dcd1c14aea1c3a2c
AsitimbayBryan2108 <99736243+AsitimbayBryan2108@users.noreply.github.com>
2022-03-03 13:09:22 -0500

    Update Tuple.py

--- Casting-en-Python/Tuple.py
+++ Casting-en-Python/Tuple.py
@@ -1,1 +1,17 @@
+#Escribir un programa que pregunte por una muestra de números, separados por comas,
+#los guarde en una tupla y muestre por pantalla su media y desviación típica.
 
+ejemplo = input("Introduce una muestra de números separados por comas: ")
+ejemplo = ejemplo.split(',')
+n = len(ejemplo)
+for i in range(n):
+    ejemplo[i] = int(ejemplo[i])
+ejemplo = tuple(ejemplo)
+sum = 0
+sumsq = 0
+for i in ejemplo:
+    sum += i
+    sumsq += i**2
+mean = sum/n
+stdev = (sumsq/n-mean**2)**(1/2)
+print('La media es', mean, ', y la desviación típica es', stdev)
