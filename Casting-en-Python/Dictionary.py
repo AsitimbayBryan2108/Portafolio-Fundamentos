9aef88c4fb36f114e0dedb5a7c5cd6772ce7ddca
AsitimbayBryan2108 <99736243+AsitimbayBryan2108@users.noreply.github.com>
2022-03-03 13:23:37 -0500

    Update Dictionary.py

--- Casting-en-Python/Dictionary.py
+++ Casting-en-Python/Dictionary.py
@@ -1,1 +1,9 @@
+my_diccionario1 = {'nombre': 'Jack', 'edad': 26}
 
+    print(mi_diccionario1['nombre'])
+    
+    print(mi_diccionario.get('edad'))
+    
+    print(mi_diccionario.get('dirección'))
+    
+    print(mi_diccionario['dirreción'])
