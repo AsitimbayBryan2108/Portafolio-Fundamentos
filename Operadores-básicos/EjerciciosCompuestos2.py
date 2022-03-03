f43f1c5415097dbd9a8bf2580893769d65bc4916
AsitimbayBryan2108 <99736243+AsitimbayBryan2108@users.noreply.github.com>
2022-03-02 23:49:24 -0500

    Create EjerciciosCompuestos2.py

---
+++ Operadores-básicos/EjerciciosCompuestos2.py
@@ -0,0 +1,10 @@
+#Variables asignadas
+a= 5
+b= 15
+#Multiplicacion de conjuntos y suma de conjuntos
+problema1 =  (a*2) + (b*2)
+print (a, '²', '+', b,'²', '=', problema1)  
+
+problema2= (a+b)*2
+print (a,'+',b,'=', problema2)
+
